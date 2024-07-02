fb89ffc9e09dc634ca83e1fc631e8b7c37d136fe
AurelioDM97 <126873779+AurelioDM97@users.noreply.github.com>
2024-07-02 23:55:12 +0200

    fixed gradle issues for sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,13 @@ plugins {
 
 android {
     namespace = "com.example.tictactoeapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.tictactoeapp"
         minSdk = 26
-        targetSdk = 33
+        //noinspection EditedTargetSdkVersion
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -37,11 +38,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation("androidx.annotation:annotation-experimental:1.4.1")
+    implementation("androidx.transition:transition:1.5.0")
+    implementation("androidx.activity:activity:1.8.0")
 }
